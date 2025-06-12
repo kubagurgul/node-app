c14325658e13570850e848b8c146dfd40df89e76
Jakub Gurgul <jakubgu@ext.inditex.com>
2025-06-12 23:04:12 +0200

    node-js server

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works XX!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
-});
-server.listen();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
