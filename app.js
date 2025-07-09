6b09ec803eb21c0a071c137780ab39a1a108f220
Jakub Gurgul <jakubgu@ext.inditex.com>
2025-07-09 23:46:34 +0200

    [express] test webhook #1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 5000;
+const PORT = 80;
 
 const AUTH_KEY = '52FB3AZWF5WMS6I';
 
